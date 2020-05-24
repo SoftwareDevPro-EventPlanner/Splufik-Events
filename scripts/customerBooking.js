54f7fc1a92dd402a95ebece700666f0b2ba1dbc2
Diamond <ifyhasdiamond@gmail.com>
2020-05-24 12:52:03 +0300

    play-around

--- scripts/customerBooking.js
+++ scripts/customerBooking.js
@@ -22,16 +22,17 @@ const setupEvents = (data) => {
         const clientsEvent = doc.data();
         const li = `
         <div>
-        <ul>
-            <li>Name: ${clientsEvent.fullName}</li>
-            <li>Date of birth: ${clientsEvent.dob}</li>
-            <li>Email Id: ${clientsEvent.email}</li>
-            <li>Date of event: ${clientsEvent.eventDate}</li>
-            <li>Type of event: ${clientsEvent.eventType}</li>
-            <li>Location of event: ${clientsEvent.eventLocation}</li>
-            <li>Phone number: ${clientsEvent.phoneNumber}</li>
-            <li>Special request: ${clientsEvent.eventDescription}</li>
-        </ul> 
+            <br/>
+            <ul>
+                <li>Name:           ${clientsEvent.fullName}</li>
+                <li>Date of birth:  ${clientsEvent.dob}</li>
+                <li>Email Id:       ${clientsEvent.email}</li>
+                <li>Date of event:  ${clientsEvent.eventDate}</li>
+                <li>Type of event:  ${clientsEvent.eventType}</li>
+                <li>Location of event: ${clientsEvent.eventLocation}</li>
+                <li>Phone number:      ${clientsEvent.phoneNumber}</li>
+                <li>Special request:   ${clientsEvent.eventDescription}</li>
+            </ul> 
         </div>
         <br/>
         <button>Confirm order</button>
